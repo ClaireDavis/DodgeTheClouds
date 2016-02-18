5ad60521477376177781dbdcf37ceb78969223c1
Claire Davis <cpv26@cornell.edu>
2016-02-17 23:37:49 -0500

    DRYifying a bit

--- BezierPaths/ViewController.m
+++ BezierPaths/ViewController.m
@@ -13,39 +13,44 @@
 #import "KTOneFingerRotationGestureRecognizer.h"
 
 @interface ViewController () <UIGestureRecognizerDelegate>
+
 @property (nonatomic, strong) CAShapeLayer *layer;
 @property (nonnull,strong) CAGradientLayer *skyBackground;
-@property (weak, nonatomic)IBOutlet
-UIView *holderView;
+
+@property (weak, nonatomic) IBOutlet UIView *holderView;
 @property (weak, nonatomic) IBOutlet firstCloudView *cloudView2;
-@property (nonatomic, strong)NSLayoutConstraint *viewLeadingConstraint;
-@property (nonatomic, strong)NSLayoutConstraint *viewTopConstraint;
-@property (nonatomic)CGFloat initialViewTopConstraint;
-@property (nonatomic  )CGFloat initialViewLeadingConstraint;
-@property (nonatomic)CGPoint viewGesturePoint;
-@property (nonatomic, strong)UIBezierPath *cloudTwo;
+
+@property (nonatomic, strong) NSLayoutConstraint *viewLeadingConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *viewTopConstraint;
+
+@property (nonatomic) CGFloat initialViewTopConstraint;
+@property (nonatomic) CGFloat initialViewLeadingConstraint;
+@property (nonatomic) CGPoint viewGesturePoint;
+
+@property (nonatomic, strong) UIBezierPath *cloudTwo;
+
 @property (weak, nonatomic) IBOutlet firstCloudView *cloudView;
-@property (nonatomic, strong)NSLayoutConstraint *cloudLeadingConstraint;
-@property (nonatomic, strong)NSLayoutConstraint *cloudTopConstraint;
-@property (nonatomic, strong)NSLayoutConstraint *cloudEndingLeadingConstraint;
-@property (nonatomic, strong)NSLayoutConstraint *cloud2TopConstraint;
-@property (nonatomic, strong)NSLayoutConstraint *cloud2LeadingConstraint;
-@property (nonatomic, strong)NSLayoutConstraint *cloud2EndingLeadingConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *cloudLeadingConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *cloudTopConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *cloudEndingLeadingConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *cloud2TopConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *cloud2LeadingConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *cloud2EndingLeadingConstraint;
 @property (weak, nonatomic) IBOutlet secondCloudView *cloudView3;
-@property (nonatomic, strong)NSLayoutConstraint *cloud3TopConstraint;
-@property (nonatomic, strong)NSLayoutConstraint *cloud3LeadingConstraint;
-@property (nonatomic, strong)NSLayoutConstraint *cloud3EndingLeadingConstraint;
-@property (nonatomic, strong)KTOneFingerRotationGestureRecognizer *oneFingerGesture;
+@property (nonatomic, strong) NSLayoutConstraint *cloud3TopConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *cloud3LeadingConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *cloud3EndingLeadingConstraint;
+
+@property (nonatomic, strong) KTOneFingerRotationGestureRecognizer *oneFingerGesture;
 @property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *panGesture;
-
 @property (weak, nonatomic) IBOutlet UILabel *youLoseLabel;
-
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
   [super viewDidLoad];
   
   self.youLoseLabel.hidden = YES;
@@ -60,45 +65,51 @@ UIView *holderView;
   
   self.skyBackground = [CAGradientLayer layer];
   self.skyBackground.frame = self.view.frame;
-  self.skyBackground.colors = [NSArray arrayWithObjects: (id)darkBlue.CGColor, (id)mediumBlue.CGColor, (id)lightBlue.CGColor, nil];
-
+  self.skyBackground.colors = [NSArray arrayWithObjects:(id)darkBlue.CGColor, (id)mediumBlue.CGColor, (id)lightBlue.CGColor, nil];
+  
   [self createView];
 }
 
--(void)createView
+- (void)createView
 {
   CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
   CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
   
   // inititalize each cloud's constraints
-  self.cloudLeadingConstraint = [self.cloudView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:screenWidth];
+  self.cloudLeadingConstraint = [self.cloudView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
+                                                                             constant:screenWidth];
   self.cloudLeadingConstraint.active = YES;
   self.cloudTopConstraint = [self.cloudView.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:screenHeight/2];
   self.cloudTopConstraint.active = YES;
-  self.cloudEndingLeadingConstraint = [self.cloudView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:-150];
+  self.cloudEndingLeadingConstraint = [self.cloudView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
+                                                                                   constant:-150];
   self.cloudEndingLeadingConstraint.active = NO;
   
-  self.cloud2LeadingConstraint = [self.cloudView2.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:screenWidth+20];
+  self.cloud2LeadingConstraint = [self.cloudView2.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
+                                                                               constant:screenWidth+20];
   self.cloud2LeadingConstraint.active = YES;
   self.cloud2TopConstraint = [self.cloudView2.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:screenHeight/3];
   self.cloud2TopConstraint.active = YES;
-  self.cloud2EndingLeadingConstraint = [self.cloudView2.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:-150];
+  self.cloud2EndingLeadingConstraint = [self.cloudView2.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
+                                                                                     constant:-150];
   self.cloud2EndingLeadingConstraint.active = NO;
   
-  self.cloud3LeadingConstraint = [self.cloudView3.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:screenWidth+30];
+  self.cloud3LeadingConstraint = [self.cloudView3.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
+                                                                               constant:screenWidth+30];
   self.cloud3LeadingConstraint.active = YES;
   self.cloud3TopConstraint = [self.cloudView3.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:screenHeight/4];
   self.cloud3TopConstraint.active = YES;
-  self.cloud3EndingLeadingConstraint = [self.cloudView3.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:-150];
+  self.cloud3EndingLeadingConstraint = [self.cloudView3.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
+                                                                                     constant:-150];
   self.cloud3EndingLeadingConstraint.active = NO;
   
   [self.view.layer insertSublayer:self.skyBackground atIndex:0];
   
   // airplane view
-  
   [self.holderView.widthAnchor constraintEqualToConstant:90].active = YES;
   [self.holderView.heightAnchor constraintEqualToConstant:54].active = YES;
-  self.viewLeadingConstraint = [self.holderView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:screenWidth/3];
+  self.viewLeadingConstraint = [self.holderView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
+                                                                             constant:screenWidth/3];
   self.viewLeadingConstraint.active = YES;
   self.initialViewLeadingConstraint = self.viewLeadingConstraint.constant;
   
@@ -108,37 +119,35 @@ UIView *holderView;
 }
 
 
--(BOOL) gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
 {
   return  YES;
 }
 
 
-- (IBAction)animateTapped:(id)sender {
-  
-  [UIView animateWithDuration:15 delay:0 options:UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionRepeat animations:^{
+- (IBAction)animateTapped:(id)sender
+{
+  [UIView animateWithDuration:15 delay:0 options:(UIViewAnimationOptionCurveEaseInOut|UIViewAnimationOptionRepeat) animations:^{
     self.cloudLeadingConstraint.active = !self.cloudLeadingConstraint.active;
     self.cloudEndingLeadingConstraint.active = !self.cloudEndingLeadingConstraint.active;
     [self.view layoutIfNeeded];
   } completion:nil];
   
   [UIView animateWithDuration:10 delay:1 options:UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionRepeat animations:^{
-    
     self.cloud2LeadingConstraint.active = !self.cloud2LeadingConstraint.active;
     self.cloud2EndingLeadingConstraint.active = !self.cloud2EndingLeadingConstraint.active;
     [self.view layoutIfNeeded];
   } completion:nil];
   
   [UIView animateWithDuration:12 delay:4 options:UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionRepeat animations:^{
-    
     self.cloud3LeadingConstraint.active = !self.cloud3LeadingConstraint.active;
     self.cloud3EndingLeadingConstraint.active = !self.cloud3EndingLeadingConstraint.active;
     [self.view layoutIfNeeded];
   } completion:nil];
 }
 
-- (IBAction)viewPanned:(UIPanGestureRecognizer *)sender {
-  
+- (IBAction)viewPanned:(UIPanGestureRecognizer *)sender
+{
   CGPoint translation = [sender translationInView:self.view];
   
   self.viewLeadingConstraint.constant = self.initialViewLeadingConstraint + translation.x;
@@ -150,41 +159,31 @@ UIView *holderView;
     self.initialViewLeadingConstraint = self.initialViewLeadingConstraint + translation.x;
   }
   
-    [self checkIfYouLose];
+  [self checkIfYouLose];
 }
 
--(void)checkIfYouLose
+- (void)checkIfYouLose
 {
-  CGFloat planeX = self.holderView.frame.origin.x;
-  CGFloat planeY = self.holderView.frame.origin.y;
-  CGPoint point1 = CGPointMake(planeX+90, planeY);
-  CGPoint point2 = CGPointMake(planeX+90, planeY);
-  CGPoint point3 = CGPointMake(planeX+90, planeY);
-  
-  CGRect cloudView1Frame = [[self.cloudView.layer presentationLayer]frame ];
-  CGPoint cloudView1Origin = cloudView1Frame.origin;
-  
-  point1.x -= (cloudView1Origin.x - self.view.frame.origin.x);
-  point1.y -= (cloudView1Origin.y - self.view.frame.origin.y);
-  
-  CGRect cloudView2Frame = [[self.cloudView2.layer presentationLayer]frame];
-  CGPoint cloudView2Origin = cloudView2Frame.origin;
-  
-  point2.x -= (cloudView2Origin.x - self.view.frame.origin.x);
-  point2.y -= (cloudView2Origin.y - self.view.frame.origin.y);
-  
-  CGRect cloudView3Frame = [[self.cloudView3.layer presentationLayer]frame];
-  CGPoint cloudView3Origin = cloudView3Frame.origin;
-  
-  point3.x -= (cloudView3Origin.x - self.view.frame.origin.x);
-  point3.y -= (cloudView3Origin.y - self.view.frame.origin.y);
-  
-  if ([self.cloudView pointInside:point1 withEvent:nil] || [self.cloudView2 pointInside:point2 withEvent:nil] || [self.cloudView3 pointInside:point3 withEvent:nil] ) {
+  CGPoint pointToCheck = self.holderView.frame.origin;
+  pointToCheck.x += 90.0f;
+  if ([self cloudView:self.cloudView containsPoint:pointToCheck] ||
+      [self cloudView:self.cloudView2 containsPoint:pointToCheck] ||
+      [self cloudView:self.cloudView3 containsPoint:pointToCheck]) {
     self.youLoseLabel.hidden  = NO;
     [self.cloudView.layer.presentationLayer removeAllAnimations];
     [self.cloudView2.layer.presentationLayer removeAllAnimations];
     [self.cloudView3.layer.presentationLayer removeAllAnimations];
   }
+}
+
+- (BOOL)cloudView:(UIView *)cloudView containsPoint:(CGPoint)pointInParent
+{
+  // shift the given point into the cloud's frame of reference, then use point inside
+  CGRect cloudFrame = [[cloudView.layer presentationLayer] frame];
+  CGPoint cloudOrigin = cloudFrame.origin;
+  CGFloat xInCloudFrameOfReference = pointInParent.x - cloudOrigin.x + self.view.frame.origin.x;
+  CGFloat yInCloudFrameOfReference = pointInParent.y - cloudOrigin.y + self.view.frame.origin.y;
+  return [cloudView pointInside:CGPointMake(xInCloudFrameOfReference, yInCloudFrameOfReference) withEvent:nil];
 }
 
 @end
